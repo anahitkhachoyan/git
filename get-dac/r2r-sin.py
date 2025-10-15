74a80f9ccfdd73086b38faef699ae27afd3856bb
anahitkhachoyan <khachoian.am@phystech.edu>
2025-10-15 23:23:57 +0300

    r2r-sin.py

--- get-dac/r2r-sin.py
+++ get-dac/r2r-sin.py
@@ -2,24 +2,22 @@ import r2r_dac as r2r
 import signal_generator as sg
 import time
 
-amplitude = 3.2
-signal_frequency = 16.5
-sampling_frequency = 1192
+amplitude = 1.0
+signal_frequency = 100.0
+sampling_frequency = 10000.0
+tim = 0.0
 
-try:
-    dac = r2r.R2RDAC([12, 20, 21, 25, 26, 17, 27, 22])
-    start_time = time.time()
-    
-    while True:
-        current_time = time.time() - start_time
-        normalized_amplitude = sg.get_sin_views_amplitude(signal_frequency, current_time)
-        signal_amplitude = normalized_amplitude * amplitude
-        dac.output(signal_amplitude)
-        sg.wait_for_sampling_period(sampling_frequency)
+if __name__ == "__main__":
+    try:
+        while True:
+            try:
+                voltage = amplitude * sg.get_sin_wave_amplitude(signal_frequency, tim)
+                r2r.dac.set_voltage(voltage)
+                sg.wait_for_sampling_period(sampling_frequency)
+                tim = tim + 1 / sampling_frequency
 
-except Exception as e:
-    print(f"Ошибка: {e}")
-finally:
-    if 'dac' in locals():
-        dac.cleanup()
-    print("Завершение работы")
+            except ValueError:
+                print("Вы ввели не число. Попробуйте ещё раз \n")
+
+    finally:
+        r2r.dac.deinit()
