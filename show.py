4a322db04bc855a61915245da17ef5b5f9cbdfbc
anahitkhachoyan <khachoian.am@phystech.edu>
2025-10-16 14:39:19 +0300

    Update show.py

--- show.py
+++ show.py
@@ -1,54 +1,52 @@
-
-import RPi.GPIO as GPIO
 import time
+import RPi.GPIO as GPIO
+import signal_generator as sg  # Исправлено: signal_Generator
 
+# Параметры сигнала
+amplitude = 1.2
+signal_frequency = 10.0
+sampling_frequency = 1000.0
+tim = 0.0
+
+# Предполагаем, что у нас есть класс PWM_DAC (нужно импортировать или определить)
 class PWM_DAC:
-    def __init__(self, gpio_pin, pwm_frequency, dynamic_range, verbose=False):  # Исправлено: pmm_frequency -> pwm_frequency
+    def __init__(self, gpio_pin, pwm_frequency, dynamic_range, verbose=False):
         self.gpio_pin = gpio_pin
-        self.frequency = pwm_frequency
-        self.dynamic_range = dynamic_range
-        self.verbose = verbose
-        
-        # Настройка GPIO
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.gpio_pin, GPIO.OUT)
+        self.pwm = GPIO.PWM(self.gpio_pin, pwm_frequency)
+        self.pwm.start(0)
+        self.dynamic_range = dynamic_range
         
-        # Создание ШИМ
-        self.pwm = GPIO.PWM(self.gpio_pin, self.frequency)
-        self.pwm.start(0)  # Запуск с 0% заполнения
-        
-        if self.verbose:
-            print(f"PWM DAC инициализирован на пине {gpio_pin}, частота {pwm_frequency} Гц, диапазон {dynamic_range} В")
-    
-    def deinit(self): 
-        self.pwm.stop()
-        GPIO.cleanup(self.gpio_pin)
-        if self.verbose:
-            print("PWM DAC деинициализирован")
-    
     def set_voltage(self, voltage):
-        if not (0.0 <= voltage <= self.dynamic_range):
-            print(f"Напряжение должно быть в диапазоне 0.0 - {self.dynamic_range:.2f} В")
-            return
-          
         duty_cycle = (voltage / self.dynamic_range) * 100
         duty_cycle = max(0, min(100, duty_cycle))
         self.pwm.ChangeDutyCycle(duty_cycle)
         
-        if self.verbose:
-            print(f"Установлено напряжение: {voltage:.2f} В, коэффициент заполнения: {duty_cycle:.1f}%")
+    def deinit(self):  # Исправлено: denrit -> deinit
+        self.pwm.stop()
+        GPIO.cleanup(self.gpio_pin)
 
-# Основной блок
 if __name__ == "__main__":
     try:
-        dac = PWM_DAC(12, 500, 3.290, True)  # Исправлено: doc -> dac
+        # Инициализация ЦАП
+        dac = PWM_DAC(12, 500, 3.3, True)  # Добавлено: создание объекта dac
+        
         while True:
             try:
-                voltage = float(input("Введите напряжение в Вольтах: "))
-                dac.set_voltage(voltage) 
+                # Генерация синусоидального сигнала
+                voltage = amplitude * sg.get_sin_wave_amplitude(signal_frequency, tim)  # Исправлено: убраны лишние скобки
+                dac.set_voltage(voltage)  # Исправлено: set_voltage/voltage -> set_voltage(voltage)
+                
+                # Ожидание следующего периода дискретизации
+                sg.wait_for_sampling_period(sampling_frequency)  # Исправлено: Sg -> sg
+                tim = tim + 1 / sampling_frequency
+                
             except ValueError:
-                print("Вы ввели не число. Попробуйте ещё раз\n") 
+                print("Вы ввели не число. Попробуйте ещё раз\n")  # Исправлен текст
+                
     except KeyboardInterrupt:
-        print("\nПрограмма завершена пользователем")
+        print("\nПрограмма завершена")
+        
     finally:
-        dac.deinit()  
+        dac.deinit()  # Исправлено: @BSc.denrit() -> dac.deinit()
