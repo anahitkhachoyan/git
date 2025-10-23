405810c30d46ee4efc388992a13aff557a859ca3
anahitkhachoyan <khachoian.am@phystech.edu>
2025-10-23 15:11:24 +0300

    Update show.py

--- show.py
+++ show.py
@@ -1,12 +1,29 @@
-class R2R_ADC:
-    def __init__(self, dynamic_range, compare_time = 0.01, verbose = False):
-        self.dynamic_range = dynamic_range
-        self.verbose = verbose
-        self.compare_time = compare_time
-        
-        self.bits_gpio = [26, 20, 19, 16, 13, 12, 25, 11]
-        self.comp_gpio = 21
+def deinit(self):
+        GPIO.output(self.bits_gpio, 0)
+        GPIO.cleanup()
+    
+    def binary_counter(self, number):
+        for i in range(8):
+            GPIO.output(self.bits_gpio[i], (number >> (7 - i)) & 1)
+    
+    def sequential_counting_adc(self):
+        for number in range(256):
+            self.binary_counter(number)
+            time.sleep(self.compare_time)
+            if GPIO.input(self.comp_gpio) == 0:
+                return number
+        return 255
+    
+    def get_sc_voltage(self):
+        number = self.sequential_counting_adc()
+        voltage = number * self.dynamic_range / 255
+        return voltage
 
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.bits_gpio, GPIO.OUT, initial = 0)
-        GPIO.setup(self.comp_gpio, GPIO.IN)
+try:
+    adc = R2R_ADC(dynamic_range=3.3)
+    while True:
+        voltage = adc.get_sc_voltage()
+        print(f"{voltage:.2f} V")
+        time.sleep(0.5)
+finally:
+    dac.deinit()
