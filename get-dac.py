0e2f3832b9b2a836dbf5d96226bcf6d607cb8d2a
Анаит Хачоян <khachoian.am@phystech.edu>
2025-10-26 18:02:59 +0300

    get-dac.py

---
+++ get-dac.py
@@ -0,0 +1,336 @@
+#signal-generator.py 
+import RPi.GPIO as IO
+import time
+import math
+
+def get_sin_wave_amplitude(freq, time):
+    y = math.sin(2 * math.pi * freq * time)
+    return (y+1)/2
+def wait_for_sampling_period(sampling_frequency):
+    time.sleep(1/sampling_frequency)
+
+def get_triangle_wave_amplitude(freq, time):
+    T=1/freq
+    x=time%T
+    if x<=T/2:
+        return (2/T)*x
+    else:
+        return 1-(2/T)*(x-T/2)
+    
+#8-bit-dac-manual.py
+import RPi.GPIO as IO
+import time
+
+DAC=[16,20,21,25,26,17,27,22]
+period=1
+value=3.3
+
+def d2b(n):
+    return [int(element) for element in bin(n)[2:].zfill(8)]
+
+def voltage_to_number(V):
+    if not(0.0 <= V <= value):
+        print(f"Напряжение выходит за динамический диапазон ЦАП (0.0-{value:.2f} В)")
+        print("устанавливаем 0.0")
+        return 0
+    return int(255*V/3.3)
+
+
+IO.setmode(IO.BCM)
+IO.setup(DAC, IO.OUT)
+IO.output(DAC, 0)
+try:
+    while True:
+        try:
+            Volt=float(input("Введите напряжение в вольтах: "))
+            nV=voltage_to_number(Volt)
+            state=d2b(nV)
+            print(f"Число на вход ЦАП {nV}, ,биты {state}")
+            for i in DAC:
+                IO.output(i, state[DAC.index(i)])
+            time.sleep(period)
+        except:
+            print("Вы ввели не число")
+        
+finally:
+    IO.output(DAC, 0)
+    IO.cleanup()
+
+
+#r2r_dac.py
+import RPi.GPIO as IO
+class R2R_DAC:
+    def __init__(self, gpio_bits, dynamic_range, verbose = False):
+        self.gpio_bits = gpio_bits
+        self.dynamic_range = dynamic_range
+        self.verbose = verbose
+        IO.setmode(IO.BCM)
+        IO.setup(self.gpio_bits, IO.OUT, initial = 0)
+    
+    def deinit(self):
+        IO.output(self.gpio_bits, 0)
+        IO.cleanup()
+    
+    def set_number(self, number):
+        return [int(element) for element in bin(number)[2:].zfill(8)]
+    
+    def set_voltage(self, V):
+        if not(0.0 <= V <= self.dynamic_range):
+            print(f"Напряжение выходит за динамический диапазон ЦАП (0.0-{self.dynamic_range:.2f} В)")
+            print("устанавливаем 0.0")
+            V = 0
+        nV=int(255*V/self.dynamic_range)
+        v_ar = self.set_number(nV)
+        for i in self.gpio_bits:
+                IO.output(i, v_ar[self.gpio_bits.index(i)])
+
+
+if __name__ == "__main__":
+    try:
+        dac = R2R_DAC([16,20,21,25,26,17,27,22], 3.183, True)
+
+        while True:
+            try:
+                voltage = float(input("Введите напряжение в вольтах: "))
+                dac.set_voltage(voltage)
+            except ValueError:
+                print("Не число!\n") 
+
+    finally:
+        dac.deinit()
+
+#pwm_dac.py
+import RPi.GPIO as IO
+class PWM_DAC:
+    def __init__(self, gpio_pin, pwm_freq, dynamic_range, verbose = False):
+        self.gpio_pin = gpio_pin
+        self.pwm_freq = pwm_freq
+        self.dynamic_range = dynamic_range
+        self.verbose = verbose
+        IO.setmode(IO.BCM)
+        IO.setup(self.gpio_pin, IO.OUT)
+        self.pwm=IO.PWM(self.gpio_pin, self.pwm_freq)
+        duty=0
+        self.pwm.start(duty)
+
+    def deinit(self):
+        IO.output(self.gpio_pin, 0)
+        IO.cleanup()
+
+    def set_voltage(self, V):
+        if not(0.0 <= V <= self.dynamic_range):
+            print(f"Напряжение выходит за динамический диапазон ЦАП (0.0-{self.dynamic_range:.2f} В)")
+            print("устанавливаем 0.0")
+            V = 0
+        duty=int(100*V/self.dynamic_range)
+        self.pwm.ChangeDutyCycle(duty)
+
+
+if __name__ == "__main__":
+    try:
+        dac = PWM_DAC(12, 500, 3.290, True)
+
+        while True:
+            try:
+                voltage = float(input("Введите напряжение в вольтах: "))
+                dac.set_voltage(voltage)
+            except ValueError:
+                print("Не число!\n") 
+
+    finally:
+        dac.deinit()    
+
+#mcp4725_driver.py
+import smbus as i2c
+class MCP4725:
+    def __init__(self, dynamic_range, address = 0X61, verbose = False):
+        self.bus = i2c.SMBus(1)
+        self.address = address
+        self.verbose = verbose
+        self.dynamic_range=dynamic_range
+        self.wm = 0
+        self.pds = 0
+
+
+    def deinit(self):
+        self.bus.close()
+
+    def set_number(self, number):
+        if not isinstance(number, int):
+            print("На вход ЦАП можно подавать только целые значения")
+        if not (0 <= number <= 4095):
+            print("Число выходит за разрядность MCP4725 (12 бит)")
+        first_byte = self.wm | self.pds | number >> 8
+        second_byte = number& 0xFF
+        self.bus.write_byte_data(self.address, first_byte, second_byte)
+
+        if self.verbose:
+            print(f"Число: {number}, отправленные по I2C данные: [0x{(self.address << 1):02X}, 0x{first_byte:02X}, 0x{second_byte:02X}]\n")
+
+    def set_voltage(self, V):
+        if not(0.0 <= V <= self.dynamic_range):
+            print(f"Напряжение выходит за динамический диапазон ЦАП (0.0-{self.dynamic_range:.2f} В)")
+            print("устанавливаем 0.0")
+            V = 0
+        nV=int(4095*V/self.dynamic_range)
+        self.set_number(nV)
+        
+if __name__ == "__main__":
+    try:
+        dac = MCP4725(5.17, 0x61, True)
+
+        while True:
+            try:
+                voltage = float(input("Введите напряжение в вольтах: "))
+                dac.set_voltage(voltage)
+            except ValueError:
+                print("Не число!\n") 
+
+    finally:
+        dac.deinit() 
+
+#r2r-sin.py
+import time
+import signal_generator as sig
+import r2r_dac as r2r
+
+amplitude = 3
+sig_freq = 20
+sampl_freq = 200
+
+
+try:
+    dc = r2r.R2R_DAC([16,20,21,25,26,17,27,22], 3.2, True)
+
+    while True:
+        try:
+
+            fx=sig.get_sin_wave_amplitude(sig_freq, time.time())
+            dc.set_voltage(fx*amplitude)
+            sig.wait_for_sampling_period(sampl_freq)
+        except ValueError:
+                print("Не число!\n") 
+
+finally:
+    dc.deinit()
+
+#pwm-sin.py
+import time
+import pwm_dac as pwm
+import signal_generator as sig
+
+amplitude = 3
+sig_freq = 5
+sampl_freq = 250
+
+try:
+    dc = pwm.PWM_DAC(12, 1000, 3.290, True)
+
+    while True:
+        try:
+
+            fx=sig.get_sin_wave_amplitude(sig_freq, time.time())
+            dc.set_voltage(fx*amplitude)
+            sig.wait_for_sampling_period(sampl_freq)
+        except ValueError:
+                print("Не число!\n") 
+
+finally:
+    dc.deinit()
+
+#mcp4725-sin.py
+import time
+import mcp4725_driver as mcp
+import signal_generator as sig
+
+amplitude = 4
+sig_freq = 5
+sampl_freq = 1000
+
+try:
+    dc = mcp.MCP4725(5.17, 0x61, True)
+
+    while True:
+        try:
+
+            fx=sig.get_sin_wave_amplitude(sig_freq, time.time())
+            dc.set_voltage(fx*amplitude)
+            sig.wait_for_sampling_period(sampl_freq)
+        except ValueError:
+                print("Не число!\n") 
+
+finally:
+    dc.deinit()
+
+#r2r-trangle.py
+import time
+import signal_generator as sig
+import r2r_dac as r2r
+
+amplitude = 3
+sig_freq = 20
+sampl_freq = 1000
+
+
+try:
+    dc = r2r.R2R_DAC([16,20,21,25,26,17,27,22], 3.2*255/256, True)
+
+    while True:
+        try:
+
+            fx=sig.get_triangle_wave_amplitude(sig_freq, time.time())
+            dc.set_voltage(fx*amplitude)
+            sig.wait_for_sampling_period(sampl_freq)
+        except ValueError:
+                print("Не число!\n") 
+
+finally:
+    dc.deinit()
+
+#pwm-trangle.py
+import time
+import pwm_dac as pwm
+import signal_generator as sig
+
+amplitude = 3
+sig_freq = 10
+sampl_freq = 1000
+
+try:
+    dc = pwm.PWM_DAC(12, 1000, 3.290, True)
+
+    while True:
+        try:
+
+            fx=sig.get_triangle_wave_amplitude(sig_freq, time.time())
+            dc.set_voltage(fx*amplitude)
+            sig.wait_for_sampling_period(sampl_freq)
+        except ValueError:
+                print("Не число!\n") 
+
+finally:
+    dc.deinit()
+
+#mcp4725-trangle.py
+import time
+import mcp4725_driver as mcp
+import signal_generator as sig
+
+amplitude = 4
+sig_freq = 10
+sampl_freq = 1000
+
+try:
+    dc = mcp.MCP4725(5.17, 0x61, True)
+
+    while True:
+        try:
+
+            fx=sig.get_triangle_wave_amplitude(sig_freq, time.time())
+            dc.set_voltage(fx*amplitude)
+            sig.wait_for_sampling_period(sampl_freq)
+        except ValueError:
+                print("Не число!\n") 
+
+finally:
+    dc.deinit()
